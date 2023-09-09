a31dc335d4118fbcdc9d8c40b228ac5689c34bd9
Robert Rice <robertrice12009@yahoo.com>
2023-09-09 15:01:00 -0500

    homework week 2

--- src/directoryLister/DirectoryLister.java
+++ src/directoryLister/DirectoryLister.java
@@ -13,22 +13,22 @@ import javax.swing.*;
  */
 public class DirectoryLister
 {
-	
+
 	// -----------------------------------------------------------------------
 	// Attributes
 	// -----------------------------------------------------------------------
-	
+
 	/** GUI used to display results */
-	private GUI gui; 
-	
+	private GUI gui;
+
 	/** base path of directory to be traversed */
 	private String basePath;
 
-	
+
 	// -----------------------------------------------------------------------
 	// Constructors
 	// -----------------------------------------------------------------------
-	
+
 	/**
 	 *	Create a new DirectoryLister that uses the specified GUI.
 	 */
@@ -36,12 +36,12 @@ public class DirectoryLister
 	{
 		this.gui = gui;
 	}
-	
-	
+
+
 	// -----------------------------------------------------------------------
 	// Methods
 	// -----------------------------------------------------------------------
-	
+
 	/**
 	 *	Allow user to select a directory for traversal.
 	 */
@@ -49,41 +49,66 @@ public class DirectoryLister
 	{
 		// clear results of any previous traversal
 		gui.resetGUI();
-		
+
 		// allow user to select a directory from the file system
 		basePath = gui.getAbsoluteDirectoryPath();
-		
+
 		// update the address label on the GUI
 		gui.setAddressLabelText(basePath);
-		
+
 		// traverse the selected directory, and display the contents
 		showDirectoryContents(basePath);
 	}
-	
-	
+
+
 	/**
 	 *	Show the directory listing.
 	 *	An error message is displayed if basePath does not represent a valid directory.
-	 * 
+	 *
 	 *	@param	basePath		the absolute path of a directory in the file system.
 	 */
 	public void showDirectoryContents(String basePath)
 	{
-		// TODO
+		File baseDir = new File(basePath);
+		if (!baseDir.isDirectory()) {
+			JOptionPane.showMessageDialog(null, "Not a valid directory.", "Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+
+		try {
+			enumerateDirectory(baseDir);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "An error occurred: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
-	
-	
+
+
 	/**
 	 *	Recursive method to enumerate the contents of a directory.
 	 *
 	 *	@param	f	directory to enumerate
 	 */
+
 	private void enumerateDirectory(File f)
 	{
-		// TODO
+		File[] files = f.listFiles();
+
+		if (files == null) return;
+
+		for (File file : files) {
+			// Update the display with all the required information
+			gui.updateListing(file.getAbsolutePath(),
+					getSizeString(file.length()),
+					file.isDirectory() ? "Folder" : "File",
+					formattedDateString(file.lastModified()));
+
+			if (file.isDirectory()) {
+				enumerateDirectory(file);
+			}
+		}
 	}
-	
-	
+
+
 	/**
 	 *	Convert a size from bytes to kilobytes, rounded down, and return an appropriate descriptive string.
 	 *	Example: 123456 bytes returns 120 KB
@@ -94,11 +119,11 @@ public class DirectoryLister
 	private String getSizeString(long size)
 	{
 		long kbSize = size / 1024;
-		
+
 		return "" + kbSize + " KB";
 	}
-	
-	
+
+
 	/**
 	 *	Return a numeric time value as a formatted date string.
 	 *
@@ -110,7 +135,7 @@ public class DirectoryLister
 	{
 		// create Date object from numeric time
 		Date d = new Date(time);
-		
+
 		// create formatter
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aaa");
 
